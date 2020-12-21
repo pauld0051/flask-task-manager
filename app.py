f3aad26c265e4ac03fef1352784f52cac94c6df7
pauld0051 <pauld0051@gmail.com>
2020-12-21 12:56:13 +0000

    Wire up task edit button with edit_task.html

--- app.py
+++ app.py
@@ -107,6 +107,13 @@ def add_task():
     return render_template("add_task.html", categories=categories)
 
 
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_task.html", task=task, categories=categories)
+
+
 @app.route("/logout")
 def logout():
     # remove user from session cookie
